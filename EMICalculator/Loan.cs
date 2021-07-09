6a314ad74e7c9b5e0b784732171756d90c57c5d5
Ranjeet Kumar <arnavraj0101@gmail.com>
2021-07-10 00:17:15 +0530

    Initial commit

---
+++ EMICalculator/Loan.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EMICalculator
+{
+    public class Loan
+    {
+        public string BankName { get; set; }
+        public string BorrowerName { get; set; }
+        public decimal Principal { get; set; }
+        public decimal Years { get; set; }
+        public decimal ROI { get; set; }
+        public decimal EmiAmount { get; set; }
+        public decimal TotalPayable { get; set; }
+        public List<decimal> EMIs = new List<decimal>();
+
+        // Creating a Loan
+        public Loan(string _bank, string _borrower, decimal _principal, decimal _years, decimal _roi)
+        {
+            this.BankName = _bank;
+            this.BorrowerName = _borrower;
+            this.Principal = _principal;
+            this.Years = _years;
+            this.ROI = _roi;
+            this.TotalPayable = this.Principal + (this.Principal * this.Years * this.ROI)/100;
+            this.EmiAmount = Math.Ceiling(this.TotalPayable / (12 * this.Years));
+
+            for(int i=1; i <= 12*this.Years; i++)
+            {
+                EMIs.Add(Math.Ceiling(this.EmiAmount));
+            }
+        }
+
+        // Making a Payment
+        public void Payment(decimal _amount, int _month)
+        {
+            _month -= 1;
+            EMIs[_month] += _amount;
+        }
+
+        
+        // Getting Balance as a String Output
+        public string getBalance(int _month)
+        {
+            //Console.WriteLine(this.ShowArray());
+            if(_month > this.EMIs.Count)
+            {
+                return " Not Valid ";
+            }else
+            {
+                decimal Sum = 0;
+                for(int i = 0; i< _month; i++)
+                {
+                    Sum += this.EMIs[i];
+                }
+                if(Sum == _month * this.EmiAmount)
+                {
+                    return this.BankName + " " + this.BorrowerName + " " + Sum + " " + (12 * this.Years - _month);
+                }
+                else
+                {
+                    return this.BankName + " " + this.BorrowerName + " " + Sum + " " + Math.Ceiling(((this.TotalPayable - Sum) / this.EmiAmount));
+
+                }
+            }
+        }
+
+        // Debug Helper Methods
+        public override string ToString()
+        {
+            return BankName + " " + BorrowerName + " " + Principal.ToString() + " " + Years.ToString() + " " + ROI.ToString() + " " + this.TotalPayable + " " + this.EmiAmount;
+        }
+
+        public string ShowArray()
+        {
+            string res = "";
+            foreach (decimal e in this.EMIs)
+            {
+                res += "," + e.ToString();
+            }
+            return res;
+        }
+    }
+}
---
+++ EMICalculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            string filePath = args[0];
+            //string filePath = "/Users/ranjeetkumar/Desktop/input.txt";
+
+            string[] commands = System.IO.File.ReadAllLines(filePath);
+            List<Loan> Loans = new List<Loan>();
+
+            foreach(string co in commands)
+            {
+                string[] attributes = co.Split();
+
+                switch (attributes[0].ToUpperInvariant())
+                {
+                    case "LOAN":
+                        // Creating Loan
+                        Loan newLoan = new Loan(attributes[1], attributes[2], decimal.Parse(attributes[3]),
+                            decimal.Parse(attributes[4]), decimal.Parse(attributes[5]));
+                        Loans.Add(newLoan);
+                        break;
+                    case "PAYMENT":
+                        // Finding the Loan
+                        Loan l = FindLoan(Loans, attributes[1], attributes[2]);
+                        if (l != null)
+                        {
+                            // Making the Payment
+                            l.Payment(decimal.Parse(attributes[3]), int.Parse(attributes[4]));
+                        }
+                        break;
+                    case "BALANCE":
+                        // Finding the Loan
+                        Loan L = FindLoan(Loans, attributes[1], attributes[2]);
+                        if( L != null)
+                        {
+                            // Getting Balance as a string and Printing in Console.
+                            Console.WriteLine(L.getBalance(int.Parse(attributes[3])));
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static Loan FindLoan(List<Loan> Loans, string _bank, string _borrower)
+        {
+            foreach (Loan l in Loans)
+            {
+                if (l.BankName == _bank && l.BorrowerName == _borrower)
+                {
+                    return l;
+                }
+            }
+            return null;
+        }
+
+    }
+}
